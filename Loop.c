9893a3d0199d53e1127a7e04d5d4328fa093d9b4
Sunil Prajapati <sunilp832@gmail.com>
2026-05-13 19:04:27 +0530

    Implement nested for loop to print stars
    Fix typo in return statement and add nested loop for printing stars.

---
+++ Loop.c
@@ -0,0 +1,15 @@
+// for loop 
+#include<stdio.h>
+int main()
+{
+  for(int i=1; i<=5; i++)
+    {
+      for(int j=1; j<=i; j++)
+        {
+          printf("*");
+        }
+    }
+  retunr 0;
+}
+
+// Output: *************** (Total 15 Times)
